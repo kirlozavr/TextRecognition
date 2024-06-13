791553421be39d0e6463a96c1d14930dca3c8b18
kirlozavr <kirill.protchenko184@gmail.com>
2024-06-13 14:41:52 +1000

    change

--- app/src/main/java/com/example/textrecognition/TextModel.kt
+++ app/src/main/java/com/example/textrecognition/TextModel.kt
@@ -1,29 +1,48 @@
 package com.example.textrecognition
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.net.Uri
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.Text
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import javax.inject.Inject
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 internal class TextModel @Inject constructor(
     @ApplicationContext private val applicationContext: Context
 ) {
 
+    private val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
     internal interface TextListener {
         fun onSuccess(imageUri: Uri, text: Text)
     }
 
     internal fun recognize(imageUri: Uri, textListener: TextListener) {
-        val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
         val image = InputImage.fromFilePath(applicationContext, imageUri)
         recognizer.process(image)
             .addOnSuccessListener { text ->
                 textListener.onSuccess(imageUri, text)
                 recognizer.close()
             }
+    }
+
+    internal suspend fun recognize(bitmap: Bitmap): Text{
+        return coroutineScope {
+            val deferred = async {
+                suspendCoroutine<Text> { continuation ->
+                    val image = InputImage.fromBitmap(bitmap, 0)
+                    recognizer.process(image).addOnSuccessListener {
+                        continuation.resume(it)
+                    }
+                }
+            }
+            deferred.await()
+        }
     }
 }
